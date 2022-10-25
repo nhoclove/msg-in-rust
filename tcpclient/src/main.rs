6fa9912d13a5d435cb87c396ccb3fb066d1087df
Nguyen <nguyenvanbac12a2@gmail.com>
2022-10-25 08:03:54 +0700

    Add basic pubsub

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -6,7 +6,8 @@ use std::{
 
 fn main() {
     let mut stream = TcpStream::connect("localhost:3000").unwrap();
-    stream.write("Hello".as_bytes()).unwrap();
+    stream.write_all(b"Hello world").unwrap();
+    stream.flush();
 
     let mut buffer = [0; 5];
     stream.read(&mut buffer).unwrap();
--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,24 +1,97 @@
-mod pubsub;
-
+use std::io::{self, BufRead};
+use std::sync::{Arc, Mutex};
 use std::{
+    fmt,
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
+
+#[derive(Debug, Clone)]
+struct PubSubError;
+
+impl fmt::Display for PubSubError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "error in PubSub")
+    }
+}
+pub struct Subscriber {
+    pub stream: Arc<Mutex<TcpStream>>,
+}
+
+impl Subscriber {
+    fn new(stream: Arc<Mutex<TcpStream>>) -> Subscriber {
+        Subscriber { stream }
+    }
+
+    fn push(&self, data: &str) {
+        self.stream
+            .lock()
+            .unwrap()
+            .write_all(data.as_bytes())
+            .unwrap();
+    }
+}
+
+pub struct PubSub {
+    pub subscribers: Vec<Subscriber>,
+}
+
+impl PubSub {
+    fn new() -> PubSub {
+        let mut subscribers: Vec<Subscriber> = Vec::new();
+
+        PubSub { subscribers }
+    }
+
+    fn subscribe(&mut self, subscriber: Subscriber) -> Result<(), PubSubError> {
+        self.subscribers.push(subscriber);
+        Ok(())
+    }
+
+    fn publish(&self, data: &str) -> Result<(), PubSubError> {
+        for subscriber in &self.subscribers {
+            subscriber.push(data);
+        }
+
+        Ok(())
+    }
+}
+
+const MESSAGE_SIZE: usize = 5;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Running on port: 3000");
-    let p = pubsub::
+    let mut pubsub = PubSub::new();
 
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
+        let mut _stream = stream.unwrap();
+
+        let sub_stream = Arc::new(Mutex::new(_stream));
+        let subscriber = Subscriber::new(Arc::clone(&sub_stream));
+        pubsub.subscribe(subscriber);
+
         println!("Connection established");
 
-        let mut buffer = [0; 1024];
-        stream.read(&mut buffer).unwrap();
+        // Store all the bytes for our received string
+        let mut received: Vec<u8> = vec![];
+        let mut rx_bytes = [0u8; MESSAGE_SIZE];
+        loop {
+            let bytes_read = sub_stream.lock().unwrap().read(&mut rx_bytes).unwrap();
+
+            received.extend_from_slice(&rx_bytes);
+
+            if bytes_read < MESSAGE_SIZE {
+                break;
+            }
+        }
+
+        let data = String::from_utf8(received)
+            .map(|msg| msg)
+            .expect("Error while concatenating string");
 
         // Write data to PubSub
-
-        // stream.write(&mut buffer).unwrap();
+        pubsub.publish(&data).unwrap();
+        println!("data: {}", data);
     }
 }
